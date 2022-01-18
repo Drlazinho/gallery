349960ba7fa18f75ca1058c7d1a310a54e2499e4
Drlazinho <lazbonfim@hotmail.com>
2022-01-18 04:13:05 -0300

    Upload randomName

--- README.md
+++ README.md
@@ -6,10 +6,31 @@ Tecnologias e dependencias - Reactjs, Typescript, Firebase, Styled-Components, U
 * Configurando o projeto com Firebase
 * Criado uma service para consultar as photos
 ~~~~javascript
-  import { ref, listAll, getDownloadURL } from "firebase/storage";
+  import { ref, listAll, getDownloadURL, uploadBytes } from "firebase/storage";
 
   // Referência ao storage e a pasta dela(segundo arg)
   const imagesFolder = ref(storage, "images");
   const photoList = await listAll(imagesFolder); //Lê os arquivos que estão na pasta
   ...
 ~~~~
+* Carregar as photos
+* Upload das Photos através da aplicação gerando um nome aleatório cada.
+~~~~javascript
+export const insert= async (file: File) => {
+  if(['image/jpeg', 'image/jpg', 'image/png'].includes(file.type)){
+
+    //Gerando um nome aleatorio para cada upload
+    let randomName = createId();
+    let newFile = ref(storage, `images/${randomName}`)
+
+    let upload = await uploadBytes(newFile, file);
+    let photoUrl = await getDownloadURL(upload.ref);
+
+    return { name: upload.ref.name, url: photoUrl } as Photo
+
+  } else {
+    return new Error('Tipo de arquivo não permitido.');
+  }
+}
+~~~~
+  - Foi utilizado o v4 da lib Uuid para geração de nome aleatório, que esta como o nome createId
--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,11 @@
 import * as C from './App.styles'
 import * as Photos from './services/photos'
-import { useState, useEffect } from 'react'
+import { useState, useEffect, FormEvent } from 'react'
 import { Photo } from './types/Photo'
 import { PhotoItem } from './components/PhotoItem'
 
 const App = () => {
+  const [uploading, setUpLoading] = useState(false);
   const [loading, setLoading] = useState(false);
   const [photos, setPhotos] = useState<Photo[]>([])
 
@@ -17,6 +18,27 @@ const App = () => {
     getPhotos();
   }, []);
 
+  const handleFormSubmit = async (e: FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+
+    const formData = new FormData(e.currentTarget);
+    const file = formData.get('image') as File;
+
+    if(file && file.size > 0) {
+      setUpLoading(true)
+      let result = await Photos.insert(file);
+      setUpLoading(false)
+
+      if(result instanceof Error) {
+        alert(`${result.name} - ${result.message}`)
+      } else {
+        let newPhotoList = [...photos];
+        newPhotoList.push(result);
+        setPhotos(newPhotoList)
+      }
+    }
+  }
+
   return (
     <C.Container>
       <C.Area>
@@ -24,7 +46,13 @@ const App = () => {
 
 
         {/* Upload */}
-        {/* Fotos */}
+        <C.UploadForm  method="POST" onSubmit={handleFormSubmit}>
+          <input type="file"  name="image"/>
+          <input type="submit" value="Enviar" />
+          {uploading && "Enviando..."}
+        </C.UploadForm>
+
+        {/*Carregar Fotos */}
         {loading &&
           <C.ScreenWarning>
             <div className='emoji'> 😸 </div>
